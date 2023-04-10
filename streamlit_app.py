6320232de469147201291f15bb511f02e5e1e5f9
orac-test <130364689+orac-test@users.noreply.github.com>
2023-04-10 13:01:43 +0100

    Update streamlit_app.py
    Added some default values to the multiselect.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ streamlit.text('🥣 🥗 🐔 🥑🍞')
 
 streamlit.text('Build your own fruit smoothie!') 
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
 
