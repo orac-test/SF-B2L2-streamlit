7887cbcb407d1c6b8939bd2eeb5c83ee260de7ed
orac-test <130364689+orac-test@users.noreply.github.com>
2023-04-10 13:05:54 +0100

    Update streamlit_app.py
    Removing the selected fruits from the dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,9 @@ streamlit.text('🥣 🥗 🐔 🥑🍞')
 streamlit.text('Build your own fruit smoothie!') 
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 
 
