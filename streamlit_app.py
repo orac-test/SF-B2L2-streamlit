331aebc4d6869e0ecc09dddbf9227c46704722ac
orac-test <130364689+orac-test@users.noreply.github.com>
2023-04-10 12:37:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+streamlit.header('Menu options - Breakfast')
+streamlit.text('Sucuk Toast')
+streamlit.text('Zeytin')
+streamlit.text('Panir')
+streamlit.text('Yumurta')
+
+
