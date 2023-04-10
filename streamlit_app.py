00cf8b23e4b61d7bd403c4f98d50e502add3adb4
orac-test <130364689+orac-test@users.noreply.github.com>
2023-04-10 12:45:22 +0100

    Update streamlit_app.py
    Added pandas, and read from S3 bucket.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,18 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Menu options - Breakfast')
-streamlit.text('Sucuk Toast')
+streamlit.text('Omega 3 & blueberry oatmeal')
 streamlit.text('Zeytin')
 streamlit.text('Panir')
 streamlit.text('Yumurta')
 
 streamlit.text('🥣 🥗 🐔 🥑🍞')
 
+streamlit.text('Build your own fruit smoothie!') 
+streamlit.dataframe(my_fruit_list)
 
