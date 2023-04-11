12bdc4ba2c45953c86ba45ee82c02f8d825b87be
orac-test <130364689+orac-test@users.noreply.github.com>
2023-04-11 10:08:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ except URLError as e:
   streamlit.error()
 
 
-streamlit.header*("The Fruit load list contains")
+streamlit.header("The Fruit load list contains")
 
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
